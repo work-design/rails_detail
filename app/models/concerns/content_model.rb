f7f24e59b408c03e691590c5528790386ba3b2c2
qinmingyuan <mingyuan0715@foxmail.com>
2016-11-27 23:32:54 +0800

    add for

--- app/models/concerns/content_model.rb
+++ app/models/concerns/content_model.rb
@@ -2,11 +2,15 @@ module ContentModel
   extend ActiveSupport::Concern
 
   included do
-    has_many :links
+    has_many :links, foreign_key: :content_id, dependent: :destroy
     Link.belongs_to :content, class_name: name
 
-    has_many :pictures
+    has_many :pictures, foreign_key: :content_id, dependent: :destroy
     Picture.belongs_to :content, class_name: name
+
+
+    accepts_nested_attributes_for :links, :pictures, allow_destroy: true
+
   end
 
 
