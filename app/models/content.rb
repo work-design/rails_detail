901edff3476f5e29b676cad253effd657fdb0778
qinmingyuan <mingyuan0715@foxmail.com>
2017-05-11 20:59:34 +0800

    add optional

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,7 @@
 class Content < ApplicationRecord
 
-  belongs_to :picture
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :picture, optional: true
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id', optional: true
   belongs_to :describe, polymorphic: true
   has_many :links
 
