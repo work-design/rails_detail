1b3cf8f9bdb13112c2171c7ba701d720b46e4b06
qinmingyuan <mingyuan0715@foxmail.com>
2017-05-18 17:58:08 +0800

    small routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,23 +5,17 @@ Rails.application.routes.draw do
 
     resources :details do
       resources :contents
-
+      resources :pictures
+      resources :items
       resources :majors do
         patch :pass, on: :member
       end
       resources :minors do
         patch :pass, on: :member
       end
-      get 'search/:q' => :search, as: :search, on: :collection
-    end
-
-    resources :lists do
-      resources :items
-    end
-    resources :knowledges do
       get :child, on: :member
     end
-    get 'detail' => 'detail#index'
+
   end
 
 end
