a66e663cb2dffe4255472a0c0e3cb3967e751374
qinmingyuan <mingyuan0715@foxmail.com>
2017-05-19 15:34:43 +0800

    create with remote added

--- app/controllers/the_detail/good_items_controller.rb
+++ app/controllers/the_detail/good_items_controller.rb
@@ -1,65 +1,65 @@
 class TheDetail::GoodItemsController < TheDetail::BaseController
-  before_action :set_good_item, only: [:show, :edit, :update, :destroy]
-  before_action :set_good
+  before_action :set_detail_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_detail
 
   def index
-    @good_items = @good.good_items
+    @detail_items = @detail.detail_items
   end
 
   def show
   end
 
   def new
-    @good_item = GoodItem.new
+    @detail_item = GoodItem.new
   end
 
   def edit
   end
 
   def create
-    @good_item = GoodItem.new(good_item_params)
+    @detail_item = GoodItem.new(detail_item_params)
 
     respond_to do |format|
-      if @good_item.save
-        format.html { redirect_to good_good_items_url(@good), notice: 'Good item was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @good_item }
+      if @detail_item.save
+        format.html { redirect_to detail_detail_items_url(@detail), notice: 'Good item was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @detail_item }
       else
         format.html { render action: 'new' }
-        format.json { render json: @good_item.errors, status: :unprocessable_entity }
+        format.json { render json: @detail_item.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def update
     respond_to do |format|
-      if @good_item.update(good_item_params)
-        format.html { redirect_to good_good_items_url(@good), notice: 'Good item was successfully updated.' }
+      if @detail_item.update(detail_item_params)
+        format.html { redirect_to detail_detail_items_url(@detail), notice: 'Good item was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @good_item.errors, status: :unprocessable_entity }
+        format.json { render json: @detail_item.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    @good_item.destroy
+    @detail_item.destroy
     respond_to do |format|
-      format.html { redirect_to good_items_url }
+      format.html { redirect_to detail_items_url }
       format.json { head :no_content }
     end
   end
 
   private
-  def set_good
-    @good = Good.find params[:good_id]
+  def set_detail
+    @detail = Good.find params[:detail_id]
   end
 
-  def set_good_item
-    @good_item = GoodItem.find(params[:id])
+  def set_detail_item
+    @detail_item = GoodItem.find(params[:id])
   end
 
-  def good_item_params
-    params[:good_item].permit(:picture, :picture_cache)
+  def detail_item_params
+    params[:detail_item].permit(:picture, :picture_cache)
   end
 end
