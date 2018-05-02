2fc7a4e72d3ae05ea1bba7ab30352949fa0cd527
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-02 09:56:33 +0800

    upgrade engine config

--- Gemfile
+++ Gemfile
@@ -1,5 +1,2 @@
 source 'https://rubygems.org'
 gemspec
-
-
-gem 'rails_com', '1.2.1'
--- lib/the_detail/engine.rb
+++ lib/the_detail/engine.rb
@@ -1,3 +1,4 @@
+require 'rails_com'
 module TheDetail
   class Engine < ::Rails::Engine
     config.eager_load_paths += Dir["#{config.root}/app/models/model_detail"]
@@ -6,6 +7,19 @@ module TheDetail
 
     initializer 'the_detail.assets.precompile' do |app|
       app.config.assets.precompile += ['the_detail_manifest.js']
+    end
+
+    initializer 'rails_com.add_generator_templates' do |app|
+      app.config.paths['lib/templates'].unshift File.expand_path('lib/templates', RailsCom::Engine.root)
+    end
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javasricpts false
+      g.javascript_engine false
+      g.helper false
+      g.jbuilder false
+      g.templates.unshift File.expand_path('lib/templates', RailsCom::Engine.root)
     end
 
   end
