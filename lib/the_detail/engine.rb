cb189c5a5f843df73f44ceb36e37916f504cc8f2
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-04 08:48:41 +0800

    fix eager load path

--- lib/the_detail/engine.rb
+++ lib/the_detail/engine.rb
@@ -1,9 +1,11 @@
 require 'rails_com'
 module TheDetail
   class Engine < ::Rails::Engine
-    config.eager_load_paths += Dir["#{config.root}/app/models/model_detail"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/model_detail/concerns"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/model_detail/contents"]
+    config.eager_load_paths += Dir[
+      "#{config.root}/app/models/the_detail",
+      "#{config.root}/app/models/the_detail/concerns",
+      "#{config.root}/app/models/the_detail/contents"
+    ]
 
     initializer 'the_detail.assets.precompile' do |app|
       app.config.assets.precompile += ['the_detail_manifest.js']
