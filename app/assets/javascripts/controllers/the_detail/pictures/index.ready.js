c16a8da4cbb323fa80b1077a96307824c856693e
qinmingyuan <mingyuan0715@foxmail.com>
2018-06-09 21:11:49 +0800

    perfect deal with img

--- app/assets/javascripts/controllers/the_detail/pictures/index.ready.js
+++ app/assets/javascripts/controllers/the_detail/pictures/index.ready.js
@@ -1,1 +1,32 @@
 //= require input-attachment
+
+class HelloController extends Stimulus.Controller {
+  greet() {
+    console.log("Hellos, Stimulus!", this.element)
+  }
+
+  connect() {
+    console.log("Connects, Stimulus!", this.element)
+    var self = this;
+    attachToInput({
+      input: 'picture_body',
+      fileInput: 'picture_file',
+      uploadUrl: window.location.pathname + '/' + self.element.dataset['id'],
+      uploadFieldName: 'picture[file]',
+      extraParams: {
+        token: '',
+        _method: 'patch'
+      },
+      extraHeaders: {
+        'Accept': 'application/json',
+        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+      },
+      onFileUploaded: function (filename) {
+        self.srcTargets[0].src = filename
+      }
+    });
+  }
+}
+HelloController.targets = ['src']
+const application = Stimulus.Application.start()
+application.register('attachment', HelloController)
