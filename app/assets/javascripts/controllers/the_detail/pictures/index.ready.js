3687d29ab1988a348674ae5a5d50fc1e9c8c5bb9
qinmingyuan <mingyuan0715@foxmail.com>
2018-06-09 20:49:46 +0800

    edit js

--- app/assets/javascripts/controllers/the_detail/pictures/index.ready.js
+++ app/assets/javascripts/controllers/the_detail/pictures/index.ready.js
@@ -1,13 +1,1 @@
 //= require input-attachment
-
-class HelloController extends Stimulus.Controller {
-  greet() {
-    console.log("Hello, Stimulus!", this.element)
-  }
-
-  connect() {
-    console.log("Connect, Stimulus!", this.element)
-  }
-}
-const application = Stimulus.Application.start()
-application.register('hello', HelloController)
