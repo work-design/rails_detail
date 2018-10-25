7e75d9fe4a1f065cced1bcb9e69e8fae88162a10
qinmingyuan <mingyuan0715@foxmail.com>
2018-10-25 09:39:08 +0800

    position

---
+++ app/controllers/detail/base_controller.rb
@@ -0,0 +1,7 @@
+class Detail::BaseController < ApplicationController
+  default_form_builder 'DetailFormBuilder' do |config|
+
+  end
+
+
+end
--- db/migrate/20150723122558_rails_detail_init.rb
+++ db/migrate/20150723122558_rails_detail_init.rb
@@ -1,11 +1,11 @@
-class CreateKnowledge < ActiveRecord::Migration[5.2]
+class RailsDetailInit < ActiveRecord::Migration[5.2]
   def change
 
     create_table :contents do |t|
       t.string :type
       t.string :title
       t.text :body, limit: 65535
-      t.integer :position, limit: 4, default: 0
+      t.integer :position, limit: 4, default: 1
       t.string :list, limit: 50
       t.references :detail, polymorphic: true
       t.references :author
