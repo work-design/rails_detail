294349aba691062fa889348cc73f7c82e8df0b9a
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-27 00:30:30 +0800

    fixed 2 errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,53 @@
 Rails.application.routes.draw do
-
-  scope :my, module: 'detail/my', as: :my, defaults: { business: 'detail', namespace: 'my' } do
-    scope path: ':detail_type/:detail_id' do
-      resource :note
-      resources :pictures
-      resources :majors do
-        patch :pass, on: :member
-      end
-      resources :minors do
-        patch :pass, on: :member
-      end
-    end
-    scope path: ':knowable_type/:knowable_id' do
-      resources :knowledges, only: [] do
+  scope RailsCom.default_routes_scope do
+    namespace 'detail', defaults: { business: 'detail' } do
+      resources :knowledges, only: [:index, :show] do
         collection do
-          get 'knowable' => :new_knowable
-          get 'search'
+          get :search
         end
-        member do
-          patch 'knowable' => :toggle_knowable
+      end
+
+      namespace :my, defaults: { namespace: 'my' } do
+        scope path: ':detail_type/:detail_id' do
+          resource :note
+          resources :pictures
+          resources :majors do
+            patch :pass, on: :member
+          end
+          resources :minors do
+            patch :pass, on: :member
+          end
+        end
+        scope path: ':knowable_type/:knowable_id' do
+          resources :knowledges, only: [] do
+            collection do
+              get 'knowable' => :new_knowable
+              get 'search'
+            end
+            member do
+              patch 'knowable' => :toggle_knowable
+            end
+          end
+        end
+        resources :knowledges
+      end
+
+      namespace :admin, defaults: { namespace: 'admin' } do
+        resources :lists do
+          resources :items
+        end
+        scope path: ':taxon_type/:taxon_id' do
+          resources :taxon_items do
+            collection do
+              get 'item' => :new_item
+              get 'list' => :new_list
+            end
+          end
+        end
+        scope path: ':entity_type/:entity_id' do
+          resources :entity_items
         end
       end
     end
-    resources :knowledges
   end
-
-  scope :admin, module: 'detail/admin', as: :admin, defaults: { business: 'detail', namespace: 'admin' } do
-    resources :lists do
-      resources :items
-    end
-    scope path: ':taxon_type/:taxon_id' do
-      resources :taxon_items do
-        collection do
-          get 'item' => :new_item
-          get 'list' => :new_list
-        end
-      end
-    end
-    scope path: ':entity_type/:entity_id' do
-      resources :entity_items
-    end
-  end
-
-  scope module: 'detail', defaults: { business: 'detail' } do
-    resources :knowledges, only: [:index, :show] do
-      collection do
-        get :search
-      end
-    end
-  end
-
 end
