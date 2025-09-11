b8ab5b302013df777708673a05456a32aecd8fca
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-11 20:51:40 +0800

    r

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,57 @@
 Rails.application.routes.draw do
-  scope RailsCom.default_routes_scope do
-    namespace 'detail', defaults: { business: 'detail' } do
-      resources :knowledges, only: [:index, :show] do
-        collection do
-          get :search
+  namespace 'detail', defaults: { business: 'detail' } do
+    resources :knowledges, only: [:index, :show] do
+      collection do
+        get :search
+      end
+    end
+
+    namespace :my, defaults: { namespace: 'my' } do
+      scope path: ':detail_type/:detail_id' do
+        resource :note
+        resources :pictures
+        resources :majors do
+          patch :pass, on: :member
+        end
+        resources :minors do
+          patch :pass, on: :member
         end
       end
-
-      namespace :my, defaults: { namespace: 'my' } do
-        scope path: ':detail_type/:detail_id' do
-          resource :note
-          resources :pictures
-          resources :majors do
-            patch :pass, on: :member
+      scope path: ':knowable_type/:knowable_id' do
+        resources :knowledges, only: [] do
+          collection do
+            post 'knowable' => :new_knowable
+            post 'search'
           end
-          resources :minors do
-            patch :pass, on: :member
-          end
-        end
-        scope path: ':knowable_type/:knowable_id' do
-          resources :knowledges, only: [] do
-            collection do
-              post 'knowable' => :new_knowable
-              post 'search'
-            end
-            member do
-              patch 'knowable' => :toggle_knowable
-            end
-          end
-        end
-        resources :knowledges do
           member do
-            post :edit_content
-            post :edit_title
-            post :cancel
+            patch 'knowable' => :toggle_knowable
           end
         end
       end
-
-      namespace :admin, defaults: { namespace: 'admin' } do
-        root 'home#index'
-        resources :lists do
-          resources :items
+      resources :knowledges do
+        member do
+          post :edit_content
+          post :edit_title
+          post :cancel
         end
-        scope path: ':taxon_type/:taxon_id' do
-          resources :taxon_items do
-            collection do
-              get 'item' => :new_item
-              get 'list' => :new_list
-            end
+      end
+    end
+
+    namespace :admin, defaults: { namespace: 'admin' } do
+      root 'home#index'
+      resources :lists do
+        resources :items
+      end
+      scope path: ':taxon_type/:taxon_id' do
+        resources :taxon_items do
+          collection do
+            get 'item' => :new_item
+            get 'list' => :new_list
           end
         end
-        scope path: ':entity_type/:entity_id' do
-          resources :entity_items
-        end
+      end
+      scope path: ':entity_type/:entity_id' do
+        resources :entity_items
       end
     end
   end
